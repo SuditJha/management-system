f03c28506e636bf52963a03d9b62b94135eea941
Sudit Kumar Jha <jsudit2@gmail.com>
2024-05-12 22:40:54 +0530

    Configured Database Connection

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// require('dotenv').config({ path: './env' })
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+import { DB_NAME } from "./constants.js";
+
+dotenv.config({
+    path: "./env"
+})
+
+await connectDB()
